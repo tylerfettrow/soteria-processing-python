a55dea67da1a6874fde6e38d302d8e8623c35b95
Tyler Fettrow <tyler.fettrow@nasa.gov>
2024-10-25 09:23:29 -0400

    cleaning up with gitignore

--- matlab/adjust_timestamps.m
+++
@@ -1,8 +0,0 @@
- function [dataoutput] = adjust_timestamps(datainput)
-timestamps_uint = uint64(datainput.UserTimeStamp);
-timestamps_datetime = datetime(double(timestamps_uint)/1e7,'ConvertFrom','epochtime','Epoch','1-Jan-0001','Format','dd-MMM-yyyy HH:mm:ss.SSSSSSSSS');
-timestamps_time = seconds(timeofday(timestamps_datetime));
-adjusted_time = timestamps_time - timestamps_time(1);
-datainput{:,1} = adjusted_time;
-dataoutput = datainput;
-end
